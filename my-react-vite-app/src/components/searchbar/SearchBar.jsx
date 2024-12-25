a2feafeaecfa1bc74db3691f5f798dc2f7d39c76
TNSHR <rohandw0@gmail.com>
2024-12-25 11:54:23 +0530

    chnages

--- my-react-vite-app/src/components/searchbar/SearchBar.jsx
+++ my-react-vite-app/src/components/searchbar/SearchBar.jsx
@@ -3,14 +3,14 @@ import "./searchBar.css";
 import logo from "../../assests/Search-icon.svg"
 
 
-const SearchBar = ({search}) => {
+const SearchBar = () => {
   return (
     <div className="container">
       <header>
       <form>
       <input className="input"
         type="text"
-       placeholder= {search}
+       placeholder= 'Search a album of your choice'
       />
        <button className="searchButton" type="submit"> 
         <img src={logo} alt="Search" className="button-icon" />
